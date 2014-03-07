1c950e1aeaf627ccb4daf9bcc4f09672bdc8f8b7
Elin Axelsson <elinaxel@gmail.com>
2014-03-07 12:22:34 +0100

    fixed antibody field in populate_lookup and added lookup to admin

--- kopf/lookup/admin.py
+++ kopf/lookup/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from lookup.models import Annotation
+
+admin.site.register(Annotation)
 
 # Register your models here.
--- kopf/populate_lookup.py
+++ kopf/populate_lookup.py
@@ -1,25 +1,15 @@
 import os
-import json
-from pprint import pprint
 
 
-def populate():# Start execution here!
+def populate():
     
-    #with open('sample_berger.json') as data_file:
-    #data = json.load(data_file)
     data=read_json('sample_berger.json')
 
     for d in data:
         if d['group']=="Berger":
-            add_annotation(scientist=d['scientist'],sample=d['id'],genotype=d['genotype'], descr=d['descr'],comments=d['comments'],tissue_type=d['tissue_type'],preparation_type=d['preparation_type'],organism=d['organism'],celltype=d['celltype'])
+            add_annotation(scientist=d['scientist'],sample=d['id'],genotype=d['genotype'], descr=d['descr'],comments=d['comments'],tissue_type=d['tissue_type'],preparation_type=d['preparation_type'],organism=d['organism'],celltype=d['celltype'],antibody=d['antibody'])
 
-
-    # Print out what we have added to the user.
-                #for c in Annotation.objects.all():
-#print "- {0}".format(str(c))
-
-
-
+    
 
 def read_json(jsonf):
     print(json)
@@ -28,17 +18,15 @@ def read_json(jsonf):
     return data
 
 
-def add_annotation(scientist, sample, genotype, descr, comments, tissue_type, preparation_type, organism, celltype):
-    obj, created = Annotation.objects.get_or_create(scientist = scientist, sample = sample, genotype = genotype, descr = descr, comments = comments, tissue_type = tissue_type, preparation_type = preparation_type, organism = organism, celltype=celltype)
+def add_annotation(scientist, sample, genotype, descr, comments, tissue_type, preparation_type, organism, celltype,antibody):
+    obj, created = Annotation.objects.get_or_create(scientist = scientist, sample = sample, genotype = genotype, descr = descr, comments = comments, tissue_type = tissue_type, preparation_type = preparation_type, organism = organism, celltype=celltype,antibody=antibody)
     return(obj)
 
 
 
-
-
-
 if __name__ == '__main__':
-    print "Starting Rango population script..."
+    print "Starting Lookup population script..."
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kopf.settings')
     from lookup.models import Annotation
+    import json
     populate()
