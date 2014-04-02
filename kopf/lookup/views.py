da4c70cdfbddea1dfaa379ecbf6b44f81497757e
Elin Axelsson <elinaxel@gmail.com>
2014-04-02 18:32:15 +0200

    updated project blog

--- kopf/lookup/views.py
+++ kopf/lookup/views.py
@@ -1,19 +1,18 @@
-from django.http import Http404
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.shortcuts import render
-from django.http import HttpResponse
-from lookup.models import Annotation, Project, Scientist, ProjectBlogg, CommentForm
 from django.core import serializers
-from django.forms.models import model_to_dict
-from itertools import chain
+from django.core.context_processors import csrf
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.core.urlresolvers import reverse
 from django.forms import ModelForm
-from django.http import HttpResponseRedirect
+from django.forms.models import model_to_dict
+from django.template import RequestContext
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, render
+from lookup.models import Annotation, Project, Scientist, ProjectBlogg, CommentForm
+from itertools import chain
+
 
 #####################
-### index
+### index page
 #####################
 
 def index(request):
@@ -25,7 +24,7 @@ def index(request):
     return render_to_response('lookup/index.html', context_dict, context)
 
 ######################
-### about
+### about page
 ######################
 
 def about(request):
@@ -50,17 +49,6 @@ def detail(request):
 
 def results(request, sample_id):
     return HttpResponse("You're looking at the results of sample %s." % sample_id)
-
-#######################
-### Projects - list all projects
-#######################
-
-def projects(request):
-    nproj = Project.objects.values_list('project_name').distinct()
-    pr = Project.objects.all()
-    #scientist = model_to_dict(Annotation.objects.all())
-    scientist = Project.objects.values()
-    return render(request, 'lookup/projects.html', {'Projectnames': nproj,'Projects': scientist})
 
 ########################
 ### Annotations
@@ -119,6 +107,20 @@ def allsamples(request):
 def cross_corr(request):
     return render(request,'lookup/croos_corr.html')
 
+#######################
+### Projects - list all projects
+#######################
+
+def projects(request):
+    #nproj = Project.objects.values_list('project_name').distinct()
+    posts = Project.objects.all() #.order_by("-project_name")
+    a = Project.objects.values_list('samples').distinct()
+    #pr = Project.objects.all()
+    #scientist = model_to_dict(Annotation.objects.all())
+    scientist = Project.objects.values()
+    return render(request, 'lookup/projects.html', {'Projectnames': posts,'Projects': scientist})
+
+
 ########################
 ## Blog
 ########################
@@ -127,36 +129,42 @@ def blog(request):
     pk = request.GET.get('project')
     posts = Project.objects.all().order_by("-project_name")
     paginator = Paginator(posts, 2)
-    
     try:
         page = int(request.GET.get("page", '1'))
     except ValueError: page = 1
-    
     try:
-        
         posts = paginator.page(page)
     except (InvalidPage, EmptyPage):
         posts = paginator.page(paginator.num_pages)
-        
-            #comment = Project(post=ProjectBlogg.objects.get(pk=pk))
     return render_to_response("lookup/blog.html", dict(posts=posts, user=request.user))
 
-from django.core.context_processors import csrf
+###########################
+## Project blog page
+###########################
 
-def project(request,pk):
-    #pk = request.GET.get('project')
+def project(request,pk): ##
     post = Project.objects.get(pk=pk)
     comments = ProjectBlogg.objects.filter(project=post)
-    d = dict(post=post, comments=comments, form=CommentForm(), user=request.user)
+    p_samples = post.samples.values_list("sample")
+    ann = Annotation.objects.filter(sample__in=p_samples)
+    
+
+    #p_samples = Project.objects.values(post.objects.values_(samples.all())  # optionally with .distinct()
+    #Kid.objects.filter(id__in=toy_owners)
+        #for i in post.samples.all():
+    #scientist = model_to_dict(Annotation.objects.all())
+    d = dict(post=post, comments=comments, form=CommentForm(), user=request.user,Anno=ann)
     d.update(csrf(request))
     return render_to_response("lookup/project_blog.html", d)
 
+###########################
+## add comments
+###########################
 
 def add_blog(request,pk):
     post = Project.objects.get(pk=pk)
     p = request.POST
     if p.has_key("body") and p["body"]:
-        author = "Anonymous"
         comment = ProjectBlogg(project=Project.objects.get(pk=pk))
         cf = CommentForm(p, instance=comment)
         comment = cf.save(commit=False)
@@ -164,8 +172,16 @@ def add_blog(request,pk):
     return(HttpResponseRedirect(reverse('lookup.views.project',args=(post.project_name,))))
 
 
+        #def getAnno(request,pk):
+        #post = Project.objects.get(pk=pk)
+
+    
 
 
+#person_specialties = person.specialties.values_list('pk', flat=True)
 
+#non_specialties = Specialties.objects.exclude(pk__in=person_specialties)
+
+#jobs = Job.objects.exclude(required_specialties__in=non_specialties)
 
 
