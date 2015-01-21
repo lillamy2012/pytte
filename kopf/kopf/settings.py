9317fa9ada77e7986bd83e6cb4fbda90287e367b
root <root@sequencing-gmi-01.oeaw.ac.at>
2015-01-21 13:22:43 +0100

    right settings.py for develop

--- kopf/kopf/settings.py
+++ kopf/kopf/settings.py
@@ -1,1 +1,122 @@
-/var/www/django/sequencing/server/kopf/kopf/settings.py
+"""
+Django settings for kopf project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '0poxzsecc9jbkcr1ad%$ke!=rnes6mm3e!n7d4eux7e2dm&5$z'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+TEMPLATE_DIRS = (
+                 # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+                 # Always use forward slashes, even on Windows.
+                 # Don't forget to use absolute paths, not relative paths.
+                 '/Users/elin.axelsson/pytte/kopf/templates/',
+                 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+                               'django.contrib.auth.context_processors.auth',
+                               'django.core.context_processors.debug',
+                               'django.core.context_processors.i18n',
+                               'django.core.context_processors.request',
+                               'django.core.context_processors.static',
+                               'django.contrib.messages.context_processors.messages',
+                               'django.core.context_processors.request'
+                               )
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+                    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+                    # Always use forward slashes, even on Windows.
+                    # Don't forget to use absolute paths, not relative paths.\
+                    os.path.join(
+                                 os.path.dirname(__file__),
+                                 'static',
+                                 ),
+                    )
+
+
+#EMAIL_HOST = 'smtp.webfaction.com'
+#EMAIL_HOST_USER = 'maxmackie'
+#EMAIL_HOST_PASSWORD = #my pass
+#EMAIL_PORT = 587
+
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'lookup',
+    'bootstrap_toolkit',
+    'django_tables2',
+    'chartit'
+                  
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'kopf.urls'
+
+WSGI_APPLICATION = 'kopf.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Europe/Vienna'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = False
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = ''
