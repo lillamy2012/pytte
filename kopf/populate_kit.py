255e90fbc034106f50213c331706f950e616ea80
Elin Axelsson <elinaxel@gmail.com>
2015-02-02 15:54:02 +0100

    Added json parser for kit (with modifications)

--- kopf/populate_kit.py
+++ kopf/populate_kit.py
@@ -1,34 +1,80 @@
 import os, re, string
 
 
-def read():
-    file = open('kit.txt', 'r')
-    l=0
-    for line in file:
-        l=l+1
-        match = re.search('Kittype', line)
-        if not match:
-            data = line.split()
-            kittype = data[0]
-            subtype = data[1]
-            comment = data[2]
-            name = data[3]
-            comp = data[4]
-            loc = data[5]
-            op = data[6]
-            pr =data[7]
-            st = data[8]
-            add_kit(kittype=kittype,subtype=subtype,comment=comment,name=name,company=comp,location=loc,opened=op,protocol=pr,stock=st)
-            print kittype
-        if match:
-            print "match"
+def populate():
+    
+    data=read_json('dump1.json')
+    
+    for e in data:
+        if e['model']=="lookup.kit":
+            name = e['pk']
+            for key, value in e.iteritems():
+                if key == 'fields':
+                    for fields, text in value.iteritems():
+                        if fields == 'kittype':
+                            kittype = text
+                        
+                        if fields == 'comment':
+                            comment = text
+                        
+                        if fields == 'company':
+                            company = text
+                        
+                        if fields == 'location':
+                            location = text
+                        
+                        if fields == 'opened':
+                            opened = text
+                        
+                        if fields == 'stock':
+                            stock = text
+                        
+                        if fields == 'active':
+                            active = text
+                        
+                        if fields == 'subtype':
+                            if text == "Rpu":
+                                subtype="Purification"
+                            elif text == "Rsq":
+                                subtype="RNAseq"
+                            elif text == "pol":
+                                subtype="Polymerase"
+                            elif text =="RTPCR":
+                                subtype="qPCR"
+                            else:
+                                 subtype=text
 
-def add_kit( kittype,subtype,comment, name, company, location,opened,protocol,stock):
-    obj, created = Kit.objects.get_or_create(kittype = kittype, subtype=subtype, comment=comment, name=name, company=company, location=location,opened=opened,protocol=protocol,stock=stock,active=True)
+            add_kit(kittype = kittype, subtype = subtype, comment = comment, name = name, company = company, location = location, opened = opened, stock = stock, active=active)
+
+
+
+
+
+def read_json(jsonf):
+    #print(json)
+    with open(jsonf, 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
+def add_kit(kittype, subtype, comment, name, company, location, opened, stock, active ):
+    obj, created = Kit.objects.get_or_create(kittype = kittype, subtype = subtype, comment = comment, name = name, company = company, location = location, opened = opened, stock = stock, active=active )
     return(obj)
+
 
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kopf.settings')
     from lookup.models import Kit
-    read()
+    import json
+    populate()
+
+
+
+
+
+
+
+
+
+
+#changes =
